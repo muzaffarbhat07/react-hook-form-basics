deaf440ef0945cf9b95e41c0e0519f0cd800bce8
Muzaffar Ahmad Bhat <bhatmuzaffar32@gmail.com>
2024-07-06 20:30:57 +0530

    material ui integration

---
+++ rhf-demo/src/components/MuiLoginForm.tsx
@@ -0,0 +1,56 @@
+import { DevTool } from "@hookform/devtools";
+import { TextField, Button, Stack } from "@mui/material";
+import { useForm } from "react-hook-form";
+
+
+type FormValues = {
+  email: string;
+  password: string;
+}
+
+const MuiLoginForm = () => {
+  const { control, register, handleSubmit, formState: { errors } } = useForm<FormValues>({
+    defaultValues: {
+      email: "",
+      password: "",
+    }
+  });
+
+  const onSubmit = (data: FormValues) => {
+    console.log('Form submitted. Form data = ', data);
+  }
+
+  return (
+    <div>
+      <h1>Mui Login Form</h1>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <Stack spacing={2} width={400}>
+          <TextField 
+            label="Email" 
+            type="email" 
+            {...register('email', { required: 'Email is required' })} 
+            error={!!errors.email}
+            helperText={errors.email?.message}
+          />
+          <TextField 
+            label="Password" 
+            type="password" 
+            {...register('password', { required: 'Password is required'})} 
+            error={!!errors.password}
+            helperText={errors.password?.message}
+          />
+          <Button 
+            variant="contained" 
+            type="submit" 
+            color="primary"
+          >
+            Log in
+          </Button>
+        </Stack>
+      </form>
+      <DevTool control={control} />
+    </div>
+  )
+}
+
+export default MuiLoginForm;
