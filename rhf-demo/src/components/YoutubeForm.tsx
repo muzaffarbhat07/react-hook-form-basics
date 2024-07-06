b81346c8c8e0649b8336ff30833090c892cbfdd1
Muzaffar Ahmad Bhat <bhatmuzaffar32@gmail.com>
2024-07-06 14:47:07 +0530

    nested objects

--- rhf-demo/src/components/YoutubeForm.tsx
+++ rhf-demo/src/components/YoutubeForm.tsx
@@ -7,6 +7,10 @@ type FormValues = {
   username: string;
   email: string;
   channel: string;
+  social: {
+    twitter: string;
+    facebook: string;
+  }
 }
 
 const YoutubeForm = () => {
@@ -15,6 +19,10 @@ const YoutubeForm = () => {
       username: '',
       email: '',
       channel: '',
+      social: {
+        twitter: '',
+        facebook: ''
+      }
     }
   });
   const { register, control, handleSubmit, formState } = form;
@@ -85,10 +93,33 @@ const YoutubeForm = () => {
             type="text" 
             id="channel" 
             {...register('channel', {
-              required: 'Channel is required',
+              required: {
+                value: true,
+                message: 'Channel is required'
+              }
             })} 
           />
           <p className="error">{errors.channel?.message}</p>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="twitter">Twitter:</label>
+          <input 
+            type="text" 
+            id="twitter" 
+            {...register('social.twitter')} 
+          />
+          {/* <p className="error">{errors.social?.twitter?.message}</p> */}
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="facebook">Facebook:</label>
+          <input 
+            type="text" 
+            id="facebook" 
+            {...register('social.facebook')}
+          />
+          {/* <p className="error">{errors.social?.facebook?.message}</p> */}
         </div>
 
         <button type="submit">Submit</button>
